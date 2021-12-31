750140328c467745936300e2e4c56f973077e070
EdgardGiorge <edgardgiorge@hotmail.com>
2021-12-30 23:46:41 -0300

    Requis.3,alteração

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,13 @@
 */
 
 const vqv = (name, age) => {
-  if (name === undefined || age === undefined) {
-    return undefined;
-  }
- 
- return `Oi, meu nome é ${name}!
- Tenho ${age} anos,
- trabalho na Trybe e mando muito em programação!
- #VQV!`;
- };
+if (!name || !age) {
+return undefined;
+}
+return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`; 
+};
  
 module.exports = vqv;
